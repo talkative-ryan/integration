b90ddebafc47a20430e994caa1e28aa8b4b36549
talkative-ryan <54412667+talkative-ryan@users.noreply.github.com>
2019-09-17 13:06:04 -0400

    Create the_math_game.py

---
+++ the_math_game.py
@@ -0,0 +1,9 @@
+# Thomas Grant
+# A math game about unusual things.
+
+userNamer = input("Hello what is your Name? ")
+print("greetings ", userNamer)
+print(5+5)
+print(6*25)
+print(pow(5,2))
+print(pow(5,1/2))
